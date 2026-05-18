809e15d2d3196ef4cc0d70135dc2315d2728bf9b
Faisal khan <pathanfaisalkhan313@gmail.com>
2026-05-18 16:46:40 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL || 'https://job-portal-backend-kna6.onrender.com'}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL || 'https://job-portal-backend-poco.onrender.com'}/api`,
 });
 
 api.interceptors.request.use((config) => {
