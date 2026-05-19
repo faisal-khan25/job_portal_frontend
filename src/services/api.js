bad37562babcaec452eb2f06b9e258e96e0e3177
faisal <pathanfaisalkhan313@gmail.com>
2026-05-19 10:08:07 +0530

    url changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL || 'https://job-portal-backend-poco.onrender.com'}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL || 'https://job-portal-frontend-avtp.onrender.com'}/api`,
 });
 
 api.interceptors.request.use((config) => {
