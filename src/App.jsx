d0c9b98ad894a6b52130f4019353a12d3245e71e
faisal <pathanfaisalkhan313@gmail.com>
2026-05-20 23:41:42 +0530

    this is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/common/ProtectedRoute';
